197b2db133dcd21a5f200a383b88c89fe53c02ba
marghepiana <mpiana@bu.edu>
2024-11-16 17:39:56 -0500

    Update accelerometer.py

--- hardware/accelerometer.py
+++ hardware/accelerometer.py
@@ -77,7 +77,7 @@ try:
         #data adapted to work well for csv 
         print(f"{data['accel']['x']:.2f}, {data['accel']['y']:.2f}, {data['accel']['z']:.2f}, {data['gyro']['x']:.2f}, {data['gyro']['y']:.2f}, {data['gyro']['z']:.2f}, {pitch:.2f}, {roll:.2f}")
 
-        #data_send = {data['accel']['x'], data['accel']['y'], data['accel']['z'], pitch, roll}
+        data_send = {data['accel']['x'], data['accel']['y'], data['accel']['z'], pitch, roll}
         #response = requests.post("http://localhost:3000/data", json=data_send)
         time.sleep(1)
 except KeyboardInterrupt:
