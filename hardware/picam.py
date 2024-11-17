4e044702f002171a8f79a889c57f9c020a1bd941
ruizmar1 <MarissaRuiz2003@gmail.com>
2024-11-17 14:11:20 -0500

    potential camera code

--- hardware/picam.py
+++ hardware/picam.py
@@ -1,7 +1,25 @@
-from picamzero import Camera
+import picamera
 from time import sleep
 
-cam = Camera()
-cam.start_preview()
-# Keep the preview window open for 5 seconds
-sleep(5)
+# Initialize the camera
+camera = picamera.PiCamera()
+
+try:
+    # Set camera properties
+    camera.resolution = (1920, 1080)  # Full HD resolution
+    camera.framerate = 30
+
+    # Start camera preview
+    camera.start_preview()
+
+    # Wait before taking a picture
+    sleep(5)
+
+    # Capture an image
+    camera.capture('/home/pi/Desktop/image.jpg')
+    print("Image captured!")
+
+finally:
+    # Close the camera
+    camera.stop_preview()
+    camera.close()
